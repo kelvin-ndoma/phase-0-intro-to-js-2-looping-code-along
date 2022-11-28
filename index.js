bbfba78dae770a10b794569de34601a51debec23
+kelvin <kevin.mutua@student.moringaschool.com>
2022-11-28 11:03:08 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solutions in this file
+function writeCards(names){
+    let messages = []
+    for(const element of names) {
+        messages.push(`Thank you, ${element}, for the wonderful surprise gift!`)
+    }
+    return messages;
+}
+function countDown(number){
+    for (let i = number; i >= 0; i--) {
+        console.log(i);
+    }
+}
